95837cb252c9d1592ba07d4e819677cecdcea15e
anvaraxadjonov1802 <axadjonov123@gmail.com>
2024-12-18 22:13:25 +0500

    fixted commit: schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, ConfigDict
 
 
 class STaskAdd(BaseModel):
@@ -9,6 +8,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class STaskId(BaseModel):
